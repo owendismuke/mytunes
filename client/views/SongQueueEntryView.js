88dc29f5e05db0c14d7076215ca0332361f6861a
Owen Dismuke <owen.dismuke@gmail.com>
2015-07-15 20:03:46 -0700

    Completed dequeue.

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -5,9 +5,9 @@ var SongQueueEntryView = Backbone.View.extend({
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
 
   events: {
-    // 'click': function() {
-    //   this.model.enqueue();
-    // }
+    'click': function() {
+      this.model.dequeue();
+    }
   },
 
   render: function(){
